80e60fb82ecb46bf4c7f382f1482bf0056607b07
Vyacheslav <dyaditsyn@gmail.com>
2020-12-22 13:00:24 +0200

    hw01 let-const and +toFixed fix

--- homework01/homework01.js
+++ homework01/homework01.js
@@ -12,47 +12,47 @@ console.log("Max price is: " + Math.max(socksPrice, jacketPrice, jeansPrice));
 console.log("Min price is: " + Math.min(socksPrice, jacketPrice, jeansPrice));
 
 // Складіть вартість всіх товарів, помістіть її в змінну та виведіть цю суму
-let sum = socksPrice + jacketPrice + jeansPrice;
+const sum = socksPrice + jacketPrice + jeansPrice;
 console.log("All goods price is: " + sum);
 
 // Відкиньте копійки у всіх товарів, потім – складіть цілу частину вартості кожного товару між собою. 
 // Округлення використовувати в МЕНЬШУ сторону.
-let intSum = Math.trunc(socksPrice) + Math.trunc(jacketPrice) + Math.trunc(jeansPrice);
+const intSum = Math.trunc(socksPrice) + Math.trunc(jacketPrice) + Math.trunc(jeansPrice);
 console.log("All goods price cents cut off before: " + intSum);
 
 // Виведіть суму товарів округлену до сотень. (Наприклад якщо вийшло 260, то виведіть 300)
 console.log("All goods price rounded to hundreds: " + Math.round(intSum/100)*100);
 
 // Виведіть булеве значення: чи є сума всіх товарів (округлена в меншу сторону) парним чи непарним числом?
-let floorSum = Math.floor(intSum/100)*100;
-let isFloorSumEven = floorSum%2 ? false : true;
+const floorSum = Math.floor(intSum/100)*100;
+const isFloorSumEven = floorSum%2 ? false : true;
 console.log("If all goods price rounded floor to hundreds is even number? " + isFloorSumEven);
 
 // Виведіть суму решти, при оплаті всіх товарів (без округлення), якщо клієнт платить 500 грн.
 const BUDGET = 500;
-let change = BUDGET - sum;
+const change = BUDGET - sum;
 console.log("Change from 500 without rounding is: " + change);
 
 // Виведіть середнє значення цін, округлене до другого знаку після коми
-let medianToCents = (sum/3).toFixed(2);
+const medianToCents = +(sum/3).toFixed(2);
 console.log("Prices median value raunded to cents: " + medianToCents);
 
 // (Більш складне) Створіть змінну, в якої збережіть випадкову знижку (використовуйте функцію Math.random).
-let discountRate = Math.floor(Math.random() * 100) + 1;
+const discountRate = Math.floor(Math.random() * 100) + 1;
 console.log("The random discount is " + discountRate + "%");
 
 // Зробіть клієнту випадкову знижку та виведіть суму до оплати округлену до 2 знаків після коми.
-let discount = sum * discountRate / 100;
-let toPay = (sum - discount).toFixed(2);
-console.log("To pay after discount: " + +toPay);
+const discount = sum * discountRate / 100;
+const toPay = +(sum - discount).toFixed(2);
+console.log("To pay after discount: " + toPay);
 
 // Виведіть чистий прибуток, якщо клієнт заплатив зі знижкою та собівартість товарів рівно в два рази нижче їх ціни?
 //(Приклад: ціна була 260.66, знижка склала 10%, клієнт заплатив на 26 меньше, собівартість = 260 / 2 -> 130. 
 // Чистий прибуток = 130 - 26 -> 104) (Приклад 2: ціна була 100, знижка склала 90%, клієнт заплатив на 90 меньше, 
 // собівартість = 100 / 2 -> 50. Чистий прибуток = 50 - 90 -> -40)
 
-let profit = (sum/2 - discount).toFixed(2); 
-console.log("The profit is: " + +profit);
+const profit = +(sum/2 - discount).toFixed(2); 
+console.log("The profit is: " + profit);
 
 //  ADVANCED
 
