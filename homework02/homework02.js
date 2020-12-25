66ad60c9704138b43b22b4d97baf1a8f9f0b441f
Vyacheslav <dyaditsyn@gmail.com>
2020-12-25 23:11:47 +0200

    hw02 v1.0

--- homework02/homework02.js
+++ homework02/homework02.js
@@ -1,76 +1,83 @@
-/* Створіть 3 змінних з наступими значеннями: 15.678, 123.965, 90.2345. 
-Ці змінні зберігають ціни на товари. Придумайте їм назви (товар можна придумати будь-який)*/
+"use strict";
+// BASE
+// ----------------------------------------------------------------------------------------------------------------------
+// Необхідно написати програму, яка виконує складання чисел від N до M 
+// (N та M – задаються користувачем), а також програма має вміти пропускати парні числа. 
+// Нижче приведено алгоритм, що потрібно зробити.
+// ----------------------------------------------------------------------------------------------------------------------
 
-const socksPrice = 15.678;
-const jacketPrice = 123.965;
-const jeansPrice = 90.2345;
+// Отримайте у користувача число(N) від якого ми будемо складати. 
+// Отримати число можна за допомогою prompt. Пам'ятайте, ЧИСЛО, не РЯДОК!
 
-// Використовуючи вбудований об'єкт Math – виведіть максимальне число
-console.log("Max price is: " + Math.max(socksPrice, jacketPrice, jeansPrice));
+// Перевірте, чи є передане значення N ЦІЛИМ числом. 
+// (Підказка: якщо при конвертації в ціле число ми отримали NaN – це число нам не підходить)
 
-// Використовуючи вбудований об'єкт Math – виведіть мінімальне число
-console.log("Min price is: " + Math.min(socksPrice, jacketPrice, jeansPrice));
+// Отримайте у користувача число(M) до якого ми будемо складати (включно). 
+// Отримати число можна за допомогою prompt. Пам'ятайте, ЧИСЛО, не РЯДОК!
 
-// Складіть вартість всіх товарів, помістіть її в змінну та виведіть цю суму
-let sum = socksPrice + jacketPrice + jeansPrice;
-console.log("All goods price is: " + sum);
+// Перевірте, чи є передане значення M ЦІЛИМ числом. 
+// (Підказка: якщо при конвертації в ціе число ми отримали NaN – це число нам не підходить)
 
-// Відкиньте копійки у всіх товарів, потім – складіть цілу частину вартості кожного товару між собою. 
-// Округлення використовувати в МЕНЬШУ сторону.
-let intSum = Math.trunc(socksPrice) + Math.trunc(jacketPrice) + Math.trunc(jeansPrice);
-console.log("All goods price cents cut off before: " + intSum);
+// Отримайте у користувача булевий параметр(true/false), який підкаже нам чи потрібно пропускати парні числа. 
+// TRUE – потрібно, FALSE – не потрібно. Використовуйте функцію confirm
 
-// Виведіть суму товарів округлену до сотень. (Наприклад якщо вийшло 260, то виведіть 300)
-console.log("All goods price rounded to hundreds: " + Math.round(intSum/100)*100);
+// Напишіть цикл, який буде складати числа від раніше отриманих N и M
 
-// Виведіть булеве значення: чи є сума всіх товарів (округлена в меншу сторону) парним чи непарним числом?
-let floorSum = Math.floor(intSum/100)*100;
-let isFloorSumEven = floorSum%2 ? false : true;
-console.log("If all goods price rounded floor to hundreds is even number? " + isFloorSumEven);
+// В циклі додайте перевірку – чи потрібно пропускати при складанні парні числа. 
+// (Використовуйте для цього if/else). Якщо парні числа потрібно пропускати не додавайте їх до суми.
 
-// Виведіть суму решти, при оплаті всіх товарів (без округлення), якщо клієнт платить 500 грн.
-const BUDGET = 500;
-let change = BUDGET - sum;
-console.log("Change from 500 without rounding is: " + change);
+// Виведіть результат
 
-// Виведіть середнє значення цін, округлене до другого знаку після коми
-let medianToCents = (sum/3).toFixed(2);
-console.log("Prices median value raunded to cents: " + medianToCents);
+// Приклад: користувач ввів такі значення для N=10, M=100, ПропускатиПарні=TRUE – сума чисел виведена на екрані буде === 2475
+// Приклад: користувач ввів такі значення для N=10, M=100, ПропускатиПарні=FALSE – сума чисел виведена на екрані буде === 5005
+// Приклад: користувач ввів такі значення для N=1, M=10, ПропускатиПарні=FALSE – сума чисел виведена на екрані буде === 55
 
-// (Більш складне) Створіть змінну, в якої збережіть випадкову знижку (використовуйте функцію Math.random).
-let discountRate = Math.floor(Math.random() * 100) + 1;
-console.log("The random discount is " + discountRate + "%");
+const firstNum = numValidation("first");
+const secondNum = numValidation("second");
 
-// Зробіть клієнту випадкову знижку та виведіть суму до оплати округлену до 2 знаків після коми.
-let discount = sum * discountRate / 100;
-let toPay = (sum - discount).toFixed(2);
-console.log("To pay after discount: " + +toPay);
+const skipEven = confirm("To skip even numbers for sum?")
 
-// Виведіть чистий прибуток, якщо клієнт заплатив зі знижкою та собівартість товарів рівно в два рази нижче їх ціни?
-//(Приклад: ціна була 260.66, знижка склала 10%, клієнт заплатив на 26 меньше, собівартість = 260 / 2 -> 130. 
-// Чистий прибуток = 130 - 26 -> 104) (Приклад 2: ціна була 100, знижка склала 90%, клієнт заплатив на 90 меньше, 
-// собівартість = 100 / 2 -> 50. Чистий прибуток = 50 - 90 -> -40)
+const sum = twoWaySum(firstNum, secondNum, skipEven);
 
-let profit = (sum/2 - discount).toFixed(2); 
-console.log("The profit is: " + +profit);
+console.log(`First number is ${firstNum}
+Second number is ${secondNum}
+To skip even numbers for sum? ${skipEven}
+The sum is ${sum}`);
 
-//  ADVANCED
+//---------------------------------------------------------------------------------------------------------------------------
 
-//  Створіть шаблонний рядок та виведіть всі обчислення вчинені в Base частині домашнього завдання у вигляді одного рядка наступного вигляду:
+function numValidation(order){
 
-//  Максимальна ціна: 9999.99
-//  Мінімальна ціна: 1111.11
-//  Вартість всіх товарів: 1345.66
-//  ...і так далі
-console.log("ADVANCED");
-console.log (`Max price is: ${Math.max(socksPrice, jacketPrice, jeansPrice)}
-Min price is: ${Math.min(socksPrice, jacketPrice, jeansPrice)}
-All goods price is: ${sum}
-All goods price cents cut off before: ${intSum}
-All goods price rounded to hundreds: ${Math.round(intSum/100)*100}
-If all goods price rounded floor to hundreds is even number? ${isFloorSumEven}
-Change from 500 without rounding is: ${change}
-Prices median value raunded to cents: ${medianToCents}
-The random discount is ${discountRate}%
-To pay after discount: ${toPay}
-The profit is: ${profit}`);
+let isNumValid = false;
+let input = "";
+
+do{
+  input = prompt(`Please enter the ${order} number to sum`);
+  if (Number.isInteger(+input) && input != "" && input != " " && input != null) {
+    isNumValid = true;
+    input = +input;
+  }
+} while (!isNumValid);
+
+return input;
+}
+
+
+function twoWaySum (num1, num2, cond){
+
+  let sum = 0;
+
+  for (let i = Math.min(num1, num2); i <= Math.max(num1, num2); i++){
+    if (cond){
+      if( i%2 !==0 ){
+        sum += i;
+      }
+    }
+    else{
+      sum += i;
+    }
+  }
+
+  return sum;
+}
+
